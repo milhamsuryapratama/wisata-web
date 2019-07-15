413a5b7b3511a73ebaffd8f30b9cf7075f85441f
milhamsuryapratama <ilhamsuryapratama.github@gmail.com>
2019-07-15 22:46:31 +0700

    logout button with dropdown

--- application/views/administrator/Template.php
+++ application/views/administrator/Template.php
@@ -105,13 +105,8 @@
                         <div class="btn-group user-helper-dropdown">
                             <i class="material-icons" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">keyboard_arrow_down</i>
                             <ul class="dropdown-menu pull-right">
-                                <li><a href="javascript:void(0);"><i class="material-icons">person</i>Profile</a></li>
-                                <li role="seperator" class="divider"></li>
-                                <li><a href="javascript:void(0);"><i class="material-icons">group</i>Followers</a></li>
-                                <li><a href="javascript:void(0);"><i class="material-icons">shopping_cart</i>Sales</a></li>
-                                <li><a href="javascript:void(0);"><i class="material-icons">favorite</i>Likes</a></li>
-                                <li role="seperator" class="divider"></li>
-                                <li><a href="javascript:void(0);"><i class="material-icons">input</i>Sign Out</a></li>
+                                
+                                <li><a href="<?=base_url()?>auth/keluar"><i class="material-icons">input</i>Keluar</a></li>
                             </ul>
                         </div>
                     </div>
